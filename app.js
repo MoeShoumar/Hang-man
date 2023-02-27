46c26c1e8bcc417bea65c1ad59880d57e149fc19
MoeShoumar <mks38@mail.aub.edu>
2023-02-27 20:55:40 +0200

    added dashes=word letters

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const input = document.getElementById('input')
 const word_div = document.querySelector('.word_div ')
 const lives = document.getElementById('lives')
 const reset = document.getElementById('reset')
-let random = ['apple', 'eat', 'horse', 'banana']
-const word = (list) => list[Math.floor(Math.random() * list.length)]
-console.log(word(random));
+let word = ['apple', 'eat', 'horse', 'banana']
+let random = word[Math.floor(Math.random() * word.length)]
+console.log(random);
+for (let i = 0; i < random.length; i++) {
+    word_div.innerHTML += '_     '
+}
+let lives_left = 3
+
+lives.innerHTML = lives_left
+
